309561bd136515585ddde55ec1da50348f3534ae
Brandon Smith <vectorcell@gmail.com>
2015-08-07 03:04:16 -0500

    install.sh now actually does something

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ cd "$dir"
 for file in $(ls); do
 	backupfile="$HOME/.${file}-backup"
 	if [ ! -e "$backupfile" ]; then
-		echo mv "$HOME/.$file" "$backupfile"
+		mv "$HOME/.$file" "$backupfile"
 	fi
-	echo cp "$file" "$HOME/.$file"
+	cp "$file" "$HOME/.$file"
 done
 
 cd
-echo source .profile
+source .profile
